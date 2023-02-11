c968b111a8dc7461e4cfde104abd64cf410f1974
Chase Brewer <jcbrew26@outlook.com>
2023-02-11 13:28:47 -0500

    fix bug

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -15,7 +15,6 @@ from decouple import config
 import os
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ip-172-31-38-121.us-east-2.compute.internal', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-18-216-81-7.us-east-2.compute.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
